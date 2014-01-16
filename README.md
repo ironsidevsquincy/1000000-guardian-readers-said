36ea23c050fe9b17805f8feb85e0bd6f25b0cd50
Darren Hurley <darren.hurley@guardian.co.uk>
2014-01-16 16:29:34 +0000

    readme; correct mongodb uri

---
+++ README.md
@@ -0,0 +1,9 @@
+Set up
+------
+
+    $ npm install
+
+To run
+------
+
+    $ MONGOLAB_URI=localhost:27017/guardian-fortunes nodemon app.js
--- app.js
+++ app.js
@@ -8,8 +8,7 @@ var express = require('express'),
     path = require('path'),
     mongo = require('mongodb'),
     monk = require('monk'),
-    db = monk('localhost:27017/guardian-fortunes');
-    // db = monk(process.env.MONGOLAB_URI);
+    db = monk(process.env.MONGOLAB_URI);
 
 // all environments
 app.set('port', process.env.PORT || 3000);
